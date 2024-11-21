aea87480e679508b3ddda7c3550aa074b41e48cb
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-21 17:49:31 +0530

    feat: configure Tailwind CSS for dark mode and enhance container settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "selector",
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         extend: {},
     },
     plugins: [],
