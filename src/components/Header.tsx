de5c2d927c07049a15ea681a85cafc57220df6a1
Mohamed Izhar <mohamed@izhar.xyz>
2024-11-22 00:07:37 +0530

    feat: add Header component for COVID Data and Population Dashboard

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-full border text-lg md:text-xl">
+            COVID Data and Population Dashboard
+        </div>
+    );
+}
